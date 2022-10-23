351e71d9ec485daf5e53e44bbf56740ee2417045
Ricardo Dantas <ricarddodantas@outlook.com>
2022-10-23 19:46:09 -0300

    Lambda function example that process event coming from API requests

---
+++ section02-api-gateway/crud-api/function.py
@@ -0,0 +1,37 @@
+import json
+
+def lambda_handler(event, context):
+    print(f'Event received: \n{json.dumps(event)}')
+    
+    try:
+        route = event['routeKey']
+
+        if route == 'GET /product':
+            body = 'Processing GET all products.'
+        elif route == 'GET /product/{id}':
+            id = event['pathParameters']['id']
+            body = f'Processing GET product ID {id}'
+        elif route == 'POST /product':
+            post_body = json.loads(event['body'])
+            body = f'Body received: {post_body}'
+        elif route == 'DELETE /product/{id}':
+            id = event['pathParameters']['id']
+            body = f'Processing DELETE product ID {id}'
+        
+        return {
+            'status_code': 200,
+            'body': {
+                'message': f'Sucessfully finished operation: {route}',
+                'body': body
+            }
+        }
+
+    except Exception as error:
+        print(error)
+        return {
+            'status_code': 400,
+            'body': {
+                'message': 'Failed to perform operation',
+                'error_message': error
+            }
+        }
